2b4995f2935d2d530393f0e866677e78e7682865
masum-ahammed <masum.ahammed@gmail.com>
2019-02-12 22:10:51 +0600

    Initial commit

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class AuthorQuiz extends Component {
+  render() {
+    return (
+      <div className="App">
+       Author Quiz
+      </div>
+    );
+  }
+}
+
+export default AuthorQuiz;

aca6194b26dea927d53c2608e028d732f772b240
masum-ahammed <masum.ahammed@gmail.com>
2019-02-16 11:12:14 +0600

    load images and data

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css';
 
-class AuthorQuiz extends Component {
-  render() {
-    return (
-      <div className="App">
-       <h1>Author Quiz</h1> 
-      </div>
-    );
-  }
+
+function Hero() {
+  return (<div className="row">
+    <div className="jambotron col-10 offset-1">
+      <h1>Author Quiz</h1>
+      <p>Select the book written by the author shown</p>
+    </div>
+  </div>);
 }
 
+function Book({title}){
+  return(<div className="answer">
+    <h4>{title}</h4>
+  </div>);
+}
+function Turn({author,books}) {
+  return (<div className="row turn" style={{ background: "white" }}>
+    <div className="col-4 offset-1">
+      <img src={author.imageUrl} className="authorimage" alt="Author"></img>
+    </div>
+    <div className="col-6">
+      {books.map((title) => <Book title={title} key={title}/>)}
+    </div>
+  </div>);
+}
+function Continue() {
+  return (<div></div>);
+}
+function Footer() {
+  return (<div id="footer" className="row">
+    <div className="col-12">
+    <p className="text-muted credit">
+          All images are from <a href="http://commons.wikimedia.org/wiki/Main_Page">Wikemedia Commons</a> and are in the public domain
+        </p>
+    </div>
+  </div>);
+}
+function AuthorQuiz({turnData}) {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData} />
+      <Continue />
+      <Footer />
+    </div>
+  );
+}
 export default AuthorQuiz;
